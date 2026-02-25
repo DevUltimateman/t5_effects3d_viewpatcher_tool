44c4e03b63147963528bfb43749288c3b83174e5
OTTO VÄNTTINEN <kingultimateman@gmail.com>
2026-02-25 20:24:49 +0200

    Add project files.

---
+++ t5_effects3d_viewpatcher_tool/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using t5_effects3d_viewpatcher_tool;
+
+
+//INTRO TEXT**/
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("T5 Effects3d ViewPatcher Tool");
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Copyright (C) 2026 by Ultimateman.\n");
+
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.WriteLine("Do you want to clear the previous dropdown list? (Y/N)");
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("It is reccommended to clear the prvious list values to avoid confusion.");
+
+
+
+//DECIDE IF USER WANTS TO CLEAR THE PREVIOUS DROPDOWN LIST
+FileHandler fH = new FileHandler();
+fH.AskForOverride();
+
+
+int x = 0;
+while( fH.CheckIfStatusChecked == false )
+{
+    if( x == 0 )
+    {
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        x++;
+    }
+    Console.WriteLine("waiting for user for anything debug");
+}
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("WE GOT FILE");
+
+
+
+fH.ReadFile( Console.ReadLine());
+
+
+
+
+//WAIT FOR A FILE TO BE DROPPED**/
+//string? clearListInput = Console.ReadLine();
+//FileHandler fH = new FileHandler();
+
+//string? path = Console.ReadLine();
+//Console.WriteLine("The filename: " + path);
+
+
+
+return 0;
+
