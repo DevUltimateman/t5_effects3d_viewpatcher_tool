1e596f1307cd4d916813ef823d4017c0fdfdae4e
OTTO VÄNTTINEN <kingultimateman@gmail.com>
2026-02-27 00:44:04 +0200

    ups

--- t5_effects3d_viewpatcher_gui_tool/MainWindow.xaml.cs
+++ t5_effects3d_viewpatcher_gui_tool/MainWindow.xaml.cs
@@ -14,24 +14,36 @@ using Microsoft.Win32;
 
 namespace t5_effects3d_viewpatcher_gui_tool
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        //grab win32 class once
+        Win32Querys win32Querys = new Win32Querys();
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Win32Querys win32Querys = new Win32Querys();
+            
+            
+
+        }
+
+        private void btnBackUp_Click(object sender, RoutedEventArgs e)
+        {
+            win32Querys.MakeBackUpOfIni();
+            txtBackUpPath.Text = win32Querys.BO_ROOT_BACKUP_INI_FILE;
+        }
+
+        private void btnFolderPicker_Click(object sender, RoutedEventArgs e)
+        {
             //get the black ops root folder path from the user using a file explorer dialog
             win32Querys.GetBlackOpsRootFolder();
             //update the on screen text box with the selected folder path
-            txtGamePath.Text = win32Querys.BOROOT;
-
+            txtGamePath.Text = win32Querys.BO_ROOT;
         }
     }
 }
--- t5_effects3d_viewpatcher_gui_tool/Win32Querys.cs
+++ t5_effects3d_viewpatcher_gui_tool/Win32Querys.cs
@@ -12,10 +12,15 @@ namespace t5_effects3d_viewpatcher_gui_tool
 {
     internal class Win32Querys
     {
-        public string BOROOT;
-        public string BOEFFECTS3DROOT;
-        public string fxViewerExe = "effectsed3.exe";
-        //public string BlackOpsRootFolder { get; set; }
+        //GLOBALS
+        public string BO_ROOT; //black ops root folder
+        public string BO_ROOT_BIN_FOLDER; //blackops bin root folder
+        public string BO_ROOT_BACKUP_INI_FILE; //backup ini file path
+
+        public string fxViewerExe = "effectsed3.exe"; //fx3d exe
+        public string fxViewerIni = "EffectsEd3--.ini"; //fx3d ini file we modify with the new paths to the assets
+
+        DateTime dates = new DateTime();
         public void GetBlackOpsRootFolder()
         {
             OpenFolderDialog fs_open = new OpenFolderDialog
@@ -23,53 +28,97 @@ namespace t5_effects3d_viewpatcher_gui_tool
                 Title = "Select the Black Ops Root Folder"
             };
 
+            //open the dialog window
             bool? ok = fs_open.ShowDialog();  // call once
 
+            //we select a folder and click
             if (ok == true)
             {
                 Console.WriteLine("Folder selected successfully.");
                 string rootFolder = fs_open.FolderName;
                 Console.WriteLine($"Selected path: {rootFolder}");
                 MessageBox.Show("SELECTED PATH?? = " + rootFolder);
-                BOROOT = rootFolder;
+                BO_ROOT = rootFolder;
                 string effects3dPath = Path.Combine(rootFolder, "bin\\");
                 string effects3dFile = effects3dPath + "\\EffectsEd3--.ini";
-                string effects3dFileStart = "EffectsEd3--.ini";
-
-                
-
-                MessageBox.Show("EFFECTS3D PATH?? = " + effects3dFile);
 
 
-                MessageBox.Show("Trying to open fx3d next debug");
+
+
+                //MessageBox.Show("EFFECTS3D PATH?? = " + effects3dFile);
+
+
+                //MessageBox.Show("Trying to open fx3d next debug");
                 //WORKS NOW AND PASSES THE RIGHT PARAMS
                 ProcessStartInfo pi = new ProcessStartInfo();
                 pi.UseShellExecute = true;
                 pi.FileName = fxViewerExe;
                 pi.WorkingDirectory = effects3dPath;
-                MessageBox.Show(pi.WorkingDirectory);
-                Process.Start(pi);
-                
+                //MessageBox.Show(pi.WorkingDirectory);
+                // Process.Start(pi);
+
             }
-            else
+
+
+            //we didnt select a folder and click cancel or close the dialog
+            else if (ok == false)
             {
+                MessageBox.Show("No folder selected.!!!!!!!!!!!!!!!!!!!!!!!!!!");
                 Console.WriteLine("No folder selected.");
                 return;
             }
-
-
-            //fs_open.Title = "Select the Black Ops Root Folder";
-            
-            
-
-            //string selectedFolder = fs_open.FolderName;
-            //Console.WriteLine("Selected folder: " + selectedFolder);
-            //BlackOpsRootFolder = selectedFolder;
         }
 
-        public string PassTheFolder( string folderPath )
+        public string PassTheFolder(string folderPath)
         {
             return folderPath;
+        }
+
+
+        public void MakeBackUpOfIni()
+        {
+            //we must check if user has selected the black ops root folder first before we can make a backup of the ini file
+            if ( BO_ROOT == null || BO_ROOT == "")
+            {
+                MessageBox.Show("Please select the Black Ops root folder first.");
+                return;
+            }
+
+            else
+            {
+                //dummy paths for testing
+                string dummyPath = Path.Combine(BO_ROOT, "bin\\EffectsEd3--.inis");
+
+                string originalPath = Path.Combine(BO_ROOT, "bin\\EffectsEd3--.ini");
+                string backupPath = Path.Combine(BO_ROOT, "bin\\EffectsEd3--.ini_backup_");
+                try
+                {
+                    if (File.Exists(originalPath)) //change to new concurrent ini file name instead of backupPath()
+                    {
+                        //dev;
+                        // backupPath = 
+                        MessageBox.Show("Backup file already exists. Creating a new backup with the current date appended to the file name.");
+                        //grab time in parsable format
+                        var timeStamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+                        File.Copy(originalPath, backupPath + timeStamp);
+                        BO_ROOT_BACKUP_INI_FILE = backupPath + timeStamp;
+                        
+                    }
+                    else if( !File.Exists(originalPath))
+                    {
+                        //DEV
+                        MessageBox.Show("No Effects3D.ini file present.\nCreating one now.");
+                        File.CreateText(originalPath).Close();
+                        File.WriteAllText(originalPath, "This is a test write.\nThis file was created as a dummy.");
+                        MessageBox.Show("Dummy ini file created at: " + dummyPath);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error creating backup: " + ex.Message);
+                }
+            }
         }
     }
 }
